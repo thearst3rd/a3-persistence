0da4233afcee559a185d96a7fe5bab27c14856ad
Terry Hearst <thearst3rd@gmail.com>
2019-09-20 00:04:57 +0000

    Functional up to the cookies

--- public/client.js
+++ public/client.js
@@ -1,42 +1,58 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-console.log('hello world :o');
-
 // our default array of dreams
-const dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
+const dreams =
+[
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
 ];
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
+const dreamsList = document.getElementById("dreams");
+const loginForm = document.forms[0];
+const usernameInput = loginForm.elements["username"];
+const passwordInput = loginForm.elements["password"];
 
 // a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
+const appendNewDream = function(dream)
+{
+  const newListItem = document.createElement("li");
   newListItem.innerHTML = dream;
   dreamsList.appendChild(newListItem);
 }
 
 // iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
+dreams.forEach( function(dream)
+{
   appendNewDream(dream);
 });
 
 // listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
+loginForm.onsubmit = function(event)
+{
   // stop our form submission from refreshing the page
   event.preventDefault();
 
   // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
+  //dreams.push(usernameInput.value);
+  //appendNewDream(usernameInput.value);
 
   // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
+  //usernameInput.value = "";
+  //usernameInput.focus();
+  
+  fetch("/login",
+  {
+    "method": "POST",
+    "body": JSON.stringify(
+    {
+      "username": usernameInput.value,
+      "password": passwordInput.value,
+    }),
+    headers: {"Content-Type": "application/json"}
+  })
+  .then(res => res.json())
+  .then(console.log)
 };
--- server.js
+++ server.js
@@ -2,21 +2,92 @@
 // where your node app starts
 
 // init project
-const express = require('express');
-const app = express();
+const express    = require("express"),
+      app        = express(),
+      session    = require("express-session"),
+      passport   = require("passport"),
+      Local      = require("passport-local").Strategy,
+      bodyParser = require("body-parser")
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
+
+app.use(bodyParser.json())
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function(request, response)
+{
+  response.sendFile(__dirname + '/views/index.html')
+})
+
+const users = 
+[
+  {"username": "thearst3rd", "password": "bruh1234"},
+  {"username": "terry", "password": "passworddd"},
+]
+
+// Setup passport authentication
+const myLocalStrategy = function(username, password, done)
+{
+  const user = users.find(usr => usr.username === username)
+  
+  if ((user !== undefined) && (user.password === password))
+  {
+    return done(null, {username, password})
+  }
+  else
+  {
+    return done(null, false, {"message": "Authentication Failed"})
+  }
+}
+
+passport.use(new Local(myLocalStrategy))
+
+app.use(session({"secret": "top 5 bruh moments", "resave": false, "saveUninitialized": false}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Listen for logging in
+app.post(
+  "/login",
+  passport.authenticate("local"),
+  function(req, res)
+  {
+    console.log("user:", req.user)
+    res.json({"status": true})
+  }
+)
+
+// Listen for authentication with cookie
+app.post(
+  "/test",
+  function(req, res)
+  {
+    console.log("Authenticate with cookie?", req.user)
+    res.json({"status": "success"})
+  }
+)
+
+passport.serializeUser((user, done) => done(null, user.username))
+passport.deserializeUser((username, done) =>
+{
+  console.log("deserializing: ", username)
+  const user = users.find(u => u.username === username)
+  
+  if (user === undefined)
+  {
+    done(null, false, {"message": "User not found, session not restored"})
+    return
+  }
+  
+  done(null, user)
+})
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(process.env.PORT, function()
+{
+  console.log("Your app is listening on port " + listener.address().port)
+})
